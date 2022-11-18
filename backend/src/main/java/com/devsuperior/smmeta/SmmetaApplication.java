856b9c8bed2ea147fb754ae7fc0ad2aa2d66bfc5
Leoni Kaian <leonikfcdegois@gmail.com>
2022-11-18 12:18:56 -0300

    Project Created

---
+++ backend/src/main/java/com/devsuperior/smmeta/SmmetaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.smmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/smmeta/SmmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.smmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
